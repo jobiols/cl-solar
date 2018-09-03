392ef06d66f10f6243393c73d6c97370eaacfb80
jobiols <jorge.obiols@gmail.com>
2018-09-03 15:40:40 -0300

    inicial

---
+++ solar_default/README.rst
@@ -0,0 +1,2 @@
+Customizacion VH Ingenieria
+===========================
---
+++ solar_default/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#
+#    Copyright (C) 2018  jeo Software  (http://www.jeosoft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+{
+    'name': 'solar',
+    'version': '11.0.0.0.0',
+    'license': 'Other OSI approved licence',
+    'category': 'Tools',
+    'summary': 'Customizacion para Panasolar International',
+    'author': 'jeo Software',
+    'depends': [
+        'sale_management',
+        'account_invoicing',
+        'purchase',
+        'project',
+        'crm',
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+
+    'port': '8069',
+    'repos': [
+        {'usr': 'jobiols', 'repo': 'cl-solar', 'branch': '11.0'},
+    ],
+    'docker': [
+        {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '11.0'},
+        {'name': 'postgres', 'usr': 'postgres', 'ver': '9.6'},
+        {'name': 'nginx', 'usr': 'nginx', 'ver': 'latest'}
+    ]
+}
